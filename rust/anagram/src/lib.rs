e81d6c61c20453ca0a3bdeac1915a287a03fec9b
Frederik Hanghøj Iversen <fhi.1990@gmail.com>
2024-01-24 14:59:12 +0100

    anagrams

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,5 +1,25 @@
 use std::collections::HashSet;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-  todo!("For the '{word}' word find anagrams among the following words: {possible_anagrams:?}");
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+  // HashSet::from_iter(
+  //   possible_anagrams
+  //     .into_iter()
+  //     .filter(|&&w| matches(word, w) )
+  // )
+  let mut h = HashSet::new();
+  for w in possible_anagrams {
+    if matches(word, w) { h.insert(*w); }
+  }
+  h
+}
+
+// a `match` b = a' /= b' && sort a == sort b
+fn matches(a: &str, b: &str) -> bool {
+  let a = a.to_lowercase();
+  let b = b.to_lowercase();
+  let mut aa = a.to_lowercase().chars().collect::<Vec<char>>();
+  aa.sort();
+  let mut bb = b.to_lowercase().chars().collect::<Vec<char>>();
+  bb.sort();
+  a != b && aa == bb
 }
