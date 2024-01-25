086cc4bd40a8aa7d54913e873288f64d2ba6ca2f
Frederik Hanghøj Iversen <fhi.1990@gmail.com>
2024-01-25 12:37:34 +0100

    reverse-string

--- rust/reverse-string/src/lib.rs
+++ rust/reverse-string/src/lib.rs
@@ -1,11 +1,3 @@
-use std::str::Chars;
-use std::iter::Rev;
-
 pub fn reverse(input: &str) -> String {
-  let chars: Rev<Chars> = input.chars().rev();
-  let mut s = String::new();
-  for c in chars {
-    s.push(c);
-  }
-  s
+  input.chars().rev().collect()
 }
