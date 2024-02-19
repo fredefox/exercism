1fbdc1b070fd087e8296dc427da89a2702241f9f
Frederik Hanghøj Iversen <fhi.1990@gmail.com>
2024-02-19 17:18:38 +0100

    pov

--- haskell/pov/src/POV.hs
+++ haskell/pov/src/POV.hs
@@ -7,26 +7,31 @@ module POV (fromPOV, tracePathBetween) where
 
 import Data.Tree (Tree(Node), Forest)
 import Control.Applicative
+import Control.Monad
 
 fromPOV :: Eq a => a -> Tree a -> Maybe (Tree a)
-fromPOV = invert id
+fromPOV a = invert id (== a)
 
-invertF :: forall f a . Alternative f => Eq a => (Forest a -> Forest a) -> a -> Forest a -> f (Tree a)
-invertF _ _ [] = empty
-invertF k a (x:xs) = invert k' a x <|> invertF k'' a xs
+invert :: forall f a . Alternative f => (Forest a -> Forest a) -> (a -> Bool) -> Tree a -> f (Tree a)
+invert k p (Node x xs) = here <|> there
+  where
+  here :: f (Tree a)
+  here = t <$ guard (p x)
+  t :: Tree a
+  t = Node x $ (xs <> k [])
+  there :: f (Tree a)
+  there = invertForest @f k' p xs
+  k' :: Forest a -> Forest a
+  k' = pure . Node x . k
+
+invertForest :: forall f a . Alternative f => (Forest a -> Forest a) -> (a -> Bool) -> Forest a -> f (Tree a)
+invertForest _ _ [] = empty
+invertForest k a (x:xs) = invert k' a x <|> invertForest k'' a xs
   where
   k' :: Forest a -> Forest a
   k' = (<> k xs)
   k'' :: Forest a -> Forest a
   k'' = k . (x:)
-
-invert :: forall f a . Alternative f => Eq a => (Forest a -> Forest a) -> a -> Tree a -> f (Tree a)
-invert k a (Node x xs)
-  | a == x = pure $ Node x $ (xs <> k [])
-  | otherwise = invertF @f k' a xs
-  where
-  k' :: Forest a -> Forest a
-  k' = pure . Node x . k
 
 path :: forall f a . Alternative f => Eq a => a -> Tree a -> f [a]
 path a (Node x xs)
@@ -34,6 +39,4 @@ path a (Node x xs)
   | otherwise = fmap (x:) $ asum $ path @f a <$> xs
 
 tracePathBetween :: Eq a => a -> a -> Tree a -> Maybe [a]
-tracePathBetween a b t = do
-  t' <- fromPOV a t
-  path b t'
+tracePathBetween a b t = fromPOV a t >>= path b
